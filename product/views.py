962ab6d5502d9a857ca8384d07e5ce105aeefe7c
vika728 <vika.sokolova728@gmail.com>
2021-03-25 18:33:43 +0600

    first commit

--- product/views.py
+++ product/views.py
@@ -11,3 +11,6 @@ def home_page(request):
 def product_list(request, slug):
     products = Product.objects.filter(category__slug=slug)
     return render(request, 'product/list.html', locals())
+def product_detail(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    return  render(request, 'product/detail.html', {'product': product})
